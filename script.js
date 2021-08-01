542e9318e29a80fcf4e899f0827fbfd471fd1a4a
KarolFradique <karol.fradique@gmail.com>
2021-08-01 12:18:40 -0300

    add arquivos

---
+++ script.js
@@ -0,0 +1,36 @@
+let digitalElement = document.querySelector('.digital')
+let sElement = document.querySelector('.p_s')
+let mElement = document.querySelector('.p_m')
+let hElement = document.querySelector('.p_h')
+
+function updateClock(){
+    let now = new Date()
+    let hour = now.getHours()
+    let minute = now.getMinutes()
+    let sec = now.getSeconds()
+
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(sec)}`
+
+    let sDeg = ((360 / 60 ) * sec) - 90
+    let mDeg = ((360 / 60 ) * minute) - 90
+    let hDeg = ((360 / 12 ) * hour) - 90
+
+
+    sElement.style.transform = `rotate(${sDeg}deg)`
+    mElement.style.transform = `rotate(${mDeg}deg)`
+    hElement.style.transform = `rotate(${hDeg}deg)`
+    
+}
+
+function fixZero(time){
+    if (time < 10) {
+        return '0'+time
+    }else{
+        return time
+    }
+
+}
+
+setInterval(updateClock, 1000)
+updateClock()
